7385f7a5c74ff49d8942820a5e69c5a113ae6a10
simeon amiase <amiase97@gmail.com>
2023-05-27 23:34:36 +0100

    finished my instagram clone

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Nav from "./components/Nav";
 import "./App.css";
 import Bio from "./components/Bio";
+import Gallery from "./components/Gallery";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Nav />
       <div className="container">
         <Bio />
+        <Gallery />
       </div>
     </>
   );
--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,28 +1,70 @@
+import { useState } from "react";
+import getPhotoUrl from 'get-photo-url'
 import profileIcon from "../assets/profileIcon.svg";
 
 const Bio = () => {
+  const [userDetails, setUserDetails] = useState({
+    name: "Dev sesugh",
+    about: "software developer @Rova",
+  });
+
+  const [editFormIsOpen, setEditFormIsOpen] = useState(false);
+  const [profilePhoto, setProfilePhoto] = useState(profileIcon)
+
+  const updateUserDetails = (event) => {
+    event.preventDefault();
+    setUserDetails({
+      name: event.target.nameOfUser.value,
+      about: event.target.aboutUser.value,
+    });
+    setEditFormIsOpen(false);
+  };
+
+  const updateProfilePhoto = async () => {
+      const newProfilePhoto = await getPhotoUrl('#profilePhotoInput')
+      setProfilePhoto(newProfilePhoto);
+    };
+
   const editForm = (
-    <form className="edit-bio-form">
-      <input type="text" id="" placeholder="Your name" />
-      <input type="text" id="" placeholder="About you" />
+    <form className="edit-bio-form" onSubmit={(e) => updateUserDetails(e)}>
+      <input type="text" id="" name="nameOfUser" placeholder="Your name" />
+      <input type="text" id="" name="aboutUser" placeholder="About you" />
       <br />
-      <button type="button" className="cancel-button">
+      <button
+        type="button"
+        className="cancel-button"
+        onClick={() => setEditFormIsOpen(false)}
+      >
         Cancel
       </button>
-      <button type="button">Save</button>
+      <button type="submit">Save</button>
     </form>
   );
 
+  const editButton = (
+    <button onClick={() => setEditFormIsOpen(true)}>Edit</button>
+  );
+
+
+
   return (
     <section className="bio">
-      <div className="profile-photo" role="button" title="click to edit photo">
-        <img src={profileIcon} alt="profile picture" />
-      </div>
+      <input type="file" accept="image/*" name="photo" id="profilePhotoInput" />
+      <label htmlFor="profilePhotoInput" onClick={updateProfilePhoto} >
+        <div
+          className="profile-photo"
+          role="button"
+          title="click to edit photo"
+        >
+          <img src={profilePhoto} alt="profile picture" />
+        </div>
+      </label>
+
       <div className="profile-info">
-        <p className="name">Simeon Amiase</p>
-        <p className="about">software developer @Rova</p>
-        <button>Edit</button>
-        {editForm}
+        <p className="name">{userDetails.name}</p>
+        <p className="about">{userDetails.about}</p>
+
+        {editFormIsOpen ? editForm : editButton}
       </div>
     </section>
   );
